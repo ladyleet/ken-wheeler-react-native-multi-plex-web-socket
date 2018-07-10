1a4af0e8d5e87a76a56bb94b71ccf1afda3354b2
Tracy Lee <tracy@thisdot.co>
2018-07-09 18:32:45 -0700

    make some improvements

--- App.js
+++ App.js
@@ -2,71 +2,71 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { webSocket} from 'rxjs/webSocket';
 import { groupBy, mergeMap, takeUntil, finalize } from 'rxjs/operators';
-import { Subscription, timer } from 'rxjs';
+import { timer } from 'rxjs';
 import kenwheeler from './ken_wheeler.png';
-const sourceImages = [kenwheeler];
 
 export default class App extends React.Component {
 
-  _socket = webSocket('ws://localhost:8080');
+  state = { randomKens: {} };
+  socket$ = webSocket('ws://localhost:8080');
 
-  state = { randomKens: [] };
-  
-  _subscription = new Subscription();
-  
-  _kenMap = new Map();
-
-  _sendKenStream() {
-    this._socket.next(JSON.stringify({
-      type: 'sub'
-    }));
+  requestKenHead() {
+    const msg = JSON.stringify({ type: 'REQUEST_KEN_HEAD' });
+    this.socket$.next(msg);
   }
 
   componentDidMount() {
-    this._subscription.add(
-      this._socket.pipe(
-        groupBy(data => data.id),
-        mergeMap(singleKenStream => 
-          singleKenStream.pipe(
-            takeUntil(
-              timer(3000),
-            ),
-            finalize(() => {
-              const dataId = singleKenStream.key;
-              this._kenMap.delete(dataId);
-            })
-          )
+    const kenHead$ = this.socket$.pipe(
+      groupBy(data => data.id),
+      mergeMap(singleKenStream => 
+        singleKenStream.pipe(
+          takeUntil(
+            timer(3000),
+          ),
+          finalize(() => {
+            const dataId = singleKenStream.key;
+            const randomKens = { ...this.state.randomKens };
+            delete randomKens[dataId];
+
+            this.setState({ randomKens });
+          })
         )
       )
-      .subscribe(data => {
-        this._kenMap.set(data.id, data);
-        this.setState({ randomKens: Array.from(this._kenMap.values()) });
-      })
     );
+
+    this.subscription = kenHead$.subscribe(data => {
+      this.setState({
+        randomKens: {
+          ...this.state.randomKens,
+          [data.id]: { id: data.id, x: data.x, y: data.y }
+        }
+      });
+    });
   }
 
   componentWillUnmount() {
-    this._subscription.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   render() {
-    
     return (
       <View style={styles.container}>
         <Text>Ken is multi-plexing!</Text>
-        {this.state.randomKens.map((randomKen, i) => <Image 
-          key={i}
-          source={sourceImages[0]}
-          style={{
-            position: 'absolute',
-            left: randomKen.x,
-            top: randomKen.y,
-          }}>
-        </Image>)}
-        <Button 
-          onPress={() => this._sendKenStream()}
+        {Object.values(this.state.randomKens).map(randomKen =>
+          <Image 
+            key={randomKen.id}
+            source={kenwheeler}
+            style={{
+              position: 'absolute',
+              left: randomKen.x,
+              top: randomKen.y,
+            }}
+          />
+        )}
+        <Button
+          onPress={() => this.requestKenHead()}
           title="add a ken bobble"
-          ></Button>
+        />
       </View>
     );
   }
